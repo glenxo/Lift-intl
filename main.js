e7c71fb64c26f22b733fa402941edf9d488a37a8
glenxo <glndxn80@gmail.com>
2022-12-15 12:26:41 -0500

    fixong the nav bar

--- main.js
+++ main.js
@@ -17,10 +17,6 @@
 })();
 
 
-let menu = document.querySelector('#menu-bars');
-let navbar = document.querySelector('.navbar');
-
-
 menu.onclick = () =>{
   menu.classList.toggle('fa-times');
   navbar.classList.toggle('active');
