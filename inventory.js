4531501d59fd72eecc69d777724e8230557a2efc
glenxo <glndxn80@gmail.com>
2022-12-17 18:49:21 -0500

    menu & parallax active

--- inventory.js
+++ inventory.js
@@ -1,56 +1,34 @@
-let my_element = document.createElement('h1');
-let my_span = document.createElement('span');
-let my_div = document.createElement('div');
-let message;
-message = 'Pagina en mantenimiento no publica';
-let button;
-let box;
+let menu = document.querySelector('#menu-btn');
+let header = document.querySelector('.header');
 
+menu.onclick = () =>{
+  menu.classList.toggle('fa-times');
+  header.classList.toggle('active');
+  document.body.classList.toggle('active');
+};
 
-alert(message);
-
-my_element.appendChild(my_span);
-
-my_element.appendChild(my_div);
-
-my_element.classList.add('custom_class');
-
-document.body.appendChild(my_element);  
-
+window.onscroll = () =>{
+  if(window.innerWidth < 1200){
+    menu.classList.remove('fa-times');
+    header.classList.remove('active');
+    document.body.classList.remove('active');
+  };
+};
 
 (function(){
 
-    var parallax = document.querySelectorAll(".banner"),
-        speed = 0.5;
-  
-    window.onscroll = function(){
-      [].slice.call(parallax).forEach(function(el,i){
-  
-        var windowYOffset = window.pageYOffset,
-            elBackgrounPos = "50% " + (windowYOffset * speed) + "px";
-  
-        el.style.backgroundPosition = elBackgrounPos;
-  
-      });
-    };
-  
-  })();
+  var parallax = document.querySelectorAll(".banner"),
+      speed = 0.5;
 
-menu.onclick = () =>{
-  searchIcon.classList.remove('fa-times');
-  searchForm.classList.remove('active');
-}
+  window.onscroll = function(){
+    [].slice.call(parallax).forEach(function(el,i){
 
-let searchIcon = document.querySelector('#search-icon');
-let searchForm = document.querySelector('.search-form');
+      var windowYOffset = window.pageYOffset,
+          elBackgrounPos = "50% " + (windowYOffset * speed) + "px";
 
-searchIcon.onclick = () =>{
-  searchIcon.classList.toggle('fa-times');
-  searchForm.classList.toggle('active');
-}
+      el.style.backgroundPosition = elBackgrounPos;
 
-window.onscroll = () =>{
-  searchIcon.classList.remove('fa-times');
-  searchForm.classList.remove('active');
-}
+    });
+  };
 
+})();
