af9bcaf01f8c2b70d2e9931016da014bbc5b3118
glenxo <glndxn80@gmail.com>
2022-12-21 16:25:07 -0500

    fixed menu autohide

--- main.js
+++ main.js
@@ -1,24 +1,34 @@
 let menu = document.querySelector('#menu-btn');
 let header = document.querySelector('.header');
 
-menu.onclick = () =>{
+menu.onclick = () => {
+  console.log('Menu clicked');
   menu.classList.toggle('fa-times');
   header.classList.toggle('active');
   document.body.classList.toggle('active');
 };
 
-window.onscroll = () =>{
-  if(window.innerWidth < 1200){
+header.addEventListener('click', event => {
+  if (event.target.matches('a, tag, anchor')) {
     menu.classList.remove('fa-times');
     header.classList.remove('active');
     document.body.classList.remove('active');
-  };
-};
+  }
+});
+
+window.addEventListener('resize', () => {
+  if (window.innerWidth < 1200) {
+    menu.classList.remove('fa-times');
+    header.classList.remove('active');
+    document.body.classList.remove('active');
+  }
+});
+
 
 
 (function(){
   var parallax = document.querySelectorAll(".contact"),
-      speed = 0.2;
+      speed = 0.3;
 
   window.onscroll = function(){
     [].slice.call(parallax).forEach(function(el,i){
